d1a89157d58a1809e17e96d905ac37557b45a8eb
Jonathan Navarrete <navakino@gmail.com>
2023-09-14 17:40:38 -0500

    Add a link that takes user to Call of Dragons page; add borders to the body.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex-row justify-center mt-6 m-auto w-1/2 text-center border-8">
+      <h1 className="border-b-2">Welcome to the Call of Dragons Resource Center</h1>
+    </main>
+  )
+}
